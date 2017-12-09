39c7c03415952fd6555e244086be5edc224bb178
krenMaksim <krenmaba@gmail.com>
2017-12-09 12:41:56 +0300

    futher learning book

---
+++ src/main/java/joshua/bloch/article30/Operation.java
@@ -0,0 +1,25 @@
+package joshua.bloch.article30;
+
+public enum Operation {
+	PLUS {
+		double apply(double x, double y) {
+			return x + y;
+		}
+	},
+	MINUS {
+		double apply(double x, double y) {
+			return x - y;
+		}
+	},
+	TIMES {
+		double apply(double x, double y) {
+			return x * y;
+		}
+	},
+	DIVIDE {
+		double apply(double x, double y) {
+			return x / y;
+		}
+	};
+	abstract double apply(double x, double y);
+}

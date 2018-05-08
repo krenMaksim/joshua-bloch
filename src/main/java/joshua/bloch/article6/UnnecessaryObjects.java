7467c8ab548c557afee677bf7874ca59de3bcfdf
krenMax <krenmv@belhard.com>
2018-05-08 09:23:16 +0300

    futher learning book

---
+++ src/main/java/joshua/bloch/article6/UnnecessaryObjects.java
@@ -0,0 +1,17 @@
+package joshua.bloch.article6;
+
+import java.util.regex.Pattern;
+
+public class UnnecessaryObjects {
+    static boolean isRomanNumeralBadVersion(String s) {
+	return s.matches("^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    }
+
+    // good version
+    private static final Pattern ROMAN = Pattern.compile("^(?=.)M*(C[MD]|D?C{0,3})" + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeral(String s) {
+	return ROMAN.matcher(s)
+	            .matches();
+    }
+}
